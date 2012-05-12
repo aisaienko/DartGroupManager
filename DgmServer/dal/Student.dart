256818bfb0c3428b38a0a7bb839a34e3045fc4ed
Isania <isaenkopp@gmail.com>
2012-05-12 19:23:34 +0300

    simple server

---
+++ DgmServer/dal/Student.dart
@@ -0,0 +1,8 @@
+class User {
+  int uid;
+  String firstName;
+  String lastName;
+  String email;
+  
+  User(this.uid, this.firstName, this.lastName, this.email);
+}
--- DgmServer/static/htdocs/dart/DgmWeb.dart
+++ DgmServer/static/htdocs/dart/DgmWeb.dart
@@ -1,4 +1,5 @@
 #import('dart:html');
+#import("dart:json");
 
 class DgmWeb {
 
@@ -6,14 +7,35 @@ class DgmWeb {
   }
 
   void run() {
-    write("Hello World!");
+    document.query('#status').innerHTML = "hello";
+    loadExistingPosts();
+  }
+  
+  void loadExistingPosts() {
+//load the posts
+    XMLHttpRequest req = new XMLHttpRequest();//.get("127.0.0.1:3030", onSuccess);
+    var url = "127.0.0.1:3030";
+    req.open("GET", url); // POST to send data
+    
+    req.on.readyStateChange.add((Event e) {
+      if (req.readyState == XMLHttpRequest.DONE &&
+          (req.status == 200 || req.status == 0)) {
+        onSuccess(req); // called when the POST successfully completes
+      }
+    });
+
+    req.send('{"name":"value"}');
+  }
+  
+  onSuccess(result) {
+    document.query('#status').innerHTML = result.textContent;
+    }
   }
 
   void write(String message) {
     // the HTML library defines a global "document" variable
-    document.query('#status').innerHTML = message;
+    document.query('#status').innerHTML = "ser";
   }
-}
 
 void main() {
   new DgmWeb().run();
